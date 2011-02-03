d0d01effa0d645168b09868241a00620dd1f4c50
Adam Rambridge <adam.rambridge@gmail.com>
2011-02-03 22:02:46 +0000

    rspec

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '3.0.3'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 
 group :test, :development do
-    gem 'rspec-rails', '>= 2.0.0.beta.22'
+    gem 'rspec-rails', '2.3.0'
 end
 
 group :cucumber do
--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,12 @@ module Grebe
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    
+    config.generators do |g|
+      g.test_framework :rspec
+    end    
+    
+    
   end
 end
