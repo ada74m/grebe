fd6646527a770f75f3d4a30b8909b2bcb559bb2c
Adam Rambridge <adam.rambridge@gmail.com>
2011-10-31 17:27:34 +0000

    started on test for pusing down negativity

--- spec/models/criteria/criterion_spec.rb
+++ spec/models/criteria/criterion_spec.rb
@@ -37,6 +37,29 @@ describe "Criterion in a hierarchy" do
   end
 
 
+end
 
+describe "Criterion hierarchy with negativity in composite nodes" do
+
+  before(:each) do
+
+    @nand = And.new :negative => true
+    @nand.children << (@or = Or.new)
+    @or.children << (@built1981 = Equals.new :model => :ship, :property => :built_year, :integer_a => 1981)
+    @or.children << (@built1974 = Equals.new :model => :ship, :property => :built_year, :integer_a => 1974)
+
+    @nand.children << (@nor = Or.new :negative => true)
+    @nor.children << (Equals.new  :model => :ship, :property => :dwt, :integer_a => 1000000)
+    @nor.children << (Equals.new  :model => :ship, :property => :dwt, :integer_a => 2000000)
+
+
+
+  end
+
+  it "should push down the negativity into the leaves" do
+
+    @nand.description.should == "not ((ship.built_year equals 1981 or ship.built_year equals 1974) and not (ship.dwt equals 1000000 or ship.dwt equals 2000000))"
+
+  end
 
 end
