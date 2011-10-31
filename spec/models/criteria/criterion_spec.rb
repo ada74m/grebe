491f6c516c4f33fa5403c0a74dfa9a52b8c83e81
Adam Rambridge <adam.rambridge@gmail.com>
2011-10-31 18:09:50 +0000

    push down negativity

--- spec/models/criteria/criterion_spec.rb
+++ spec/models/criteria/criterion_spec.rb
@@ -43,23 +43,22 @@ describe "Criterion hierarchy with negativity in composite nodes" do
 
   before(:each) do
 
-    @nand = And.new :negative => true
-    @nand.children << (@or = Or.new)
+    @root = And.new :negative => true
+    @root.children << (@or = Or.new)
     @or.children << (Equals.new :model => :ship, :property => :built_year, :integer_a => 1981)
-    @or.children << (Equals.new :model => :ship, :property => :built_year, :integer_a => 1974)
+    @or.children << (Equals.new :model => :ship, :property => :built_year, :integer_a => 1974, :negative => true)
 
-    @nand.children << (@nor = Or.new :negative => true)
+    @root.children << (@nor = Or.new :negative => true)
     @nor.children << (Equals.new  :model => :ship, :property => :dwt, :integer_a => 1000000)
     @nor.children << (Equals.new  :model => :ship, :property => :dwt, :integer_a => 2000000)
 
+    @root.description.should == "not ((ship.built_year equals 1981 or ship.built_year does not equal 1974) and not (ship.dwt equals 1000000 or ship.dwt equals 2000000))"
 
 
   end
 
   it "should push down the negativity into the leaves" do
-
-    @nand.description.should == "not ((ship.built_year equals 1981 or ship.built_year equals 1974) and not (ship.dwt equals 1000000 or ship.dwt equals 2000000))"
-
+    @root.push_down_negativity
   end
 
 end
